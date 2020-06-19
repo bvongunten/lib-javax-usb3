12f8c33a98cb2e12be5aa25f047b1815e701ac83
Bernhard von Gunten <bvg@nostromo.ch>
2020-06-20 01:18:55 +0200

    Jigsaw update, Windows 2nd library load

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module ch.nostromo.tiffanys.engines.commons {
+    requires java.logging;
+    requires ch.nostromo.tiffanys.commons;
+    exports ch.nostromo.tiffanys.engines.commons;
+    exports ch.nostromo.tiffanys.engines.commons.events;
+    exports ch.nostromo.tiffanys.engines.commons.opening;
+}
